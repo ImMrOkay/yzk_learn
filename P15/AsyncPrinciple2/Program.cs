3d3b8c775dcc13cdcb60267e10aa55d00b1b440b
HG <yan_yu_lv_ji@163.com>
2022-12-05 12:05:10 +0800

    新增P15：await前和await后的线程可能是不一样的！

---
+++ P15/AsyncPrinciple2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncPrinciple2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Console.WriteLine($"程序启动时的线程Id：{Thread.CurrentThread.ManagedThreadId}");
+            // 创建文本文件
+            string fileName = @"./a.txt";
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName);
+                Thread.Sleep(1000);
+            }
+
+            // 构造一个大的字符串，以写入到文本文件
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < 5000000; i++)
+            {
+                stringBuilder.Append("Hello!\n");
+            }
+
+            // 异步写入,await直到写完;
+            Console.WriteLine($"await写入前的线程Id：{Thread.CurrentThread.ManagedThreadId}");
+            await File.WriteAllTextAsync(fileName, stringBuilder.ToString());
+            Console.WriteLine($"await写入后的线程Id：{Thread.CurrentThread.ManagedThreadId}");
+
+            // 下面这段代码执行起来就好像在执行for循环一样
+            Console.WriteLine($"await读取前的线程Id：{Thread.CurrentThread.ManagedThreadId}");
+            string s = await File.ReadAllTextAsync(fileName);
+            Console.WriteLine($"await读取后的线程Id：{Thread.CurrentThread.ManagedThreadId}");
+
+            //Console.WriteLine(s);
+            Console.WriteLine($"程序结束时的线程Id：{Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
